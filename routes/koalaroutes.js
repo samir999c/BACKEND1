c8a4a7ef83d6debd1f1ffce7db659a85cee5eca3
samir999c <samirchudal00@gmail.com>
2025-09-14 18:42:46 +1000

    a

--- routes/koalaroutes.js
+++ routes/koalaroutes.js
@@ -62,7 +62,7 @@ router.post("/flights", authMiddleware, async (req, res) => {
 
     const paramsForSignature = {
       marker: MARKER,
-      host: req.headers.host || "localhost",
+      host: "api.travelpayouts.com",
       user_ip: req.ip || req.socket.remoteAddress || "127.0.0.1",
       locale: "en",
       trip_class: trip_class.toUpperCase(),
